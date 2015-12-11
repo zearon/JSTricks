cbe2cef066557e9941501704fc1b6c4023d0c8de
ZHIYUAN GONG <register@zearon.com>
2015-12-11 21:54:48 +0800

    Workable version. 1. Add a simple template engine templating.js and move some code snippet into codesnippet.js , and template string into templates.js 2. Change template of content script. 3. Add a select box in content script editor page to generate script according to content script templates. 4. Rename button can rename Module ID of module defined in content script.

--- templates.js
+++ templates.js
@@ -8,20 +8,18 @@ var template_site_script =
 });
 `;
 
-
 var template_content_script_run =
 `/* run function defines an entry point. It takes two parameters. Parameter 1 is the dependency list of this module.
- * Every element of this list is an ID of a dependent script. If the script is a dynamic content script, its ID
- * is the same with its name, except that it starts with a #. If there is no dependency, the first parameter can be omitted.
  * Parameter 2 is the function to to be called, and every dependency corresponds to a parameter of this function.
  */
-run(["jquery", {{name}}], function($) {
-  // Your code here.
+run(["jquery", "#{{name}}"], function($, obj) {
+  // Call the do method defined in the module above.
+  obj.do();
 });
 `;
 
 
-var template_content_script_module =
+var template_content_script_module_object =
 `/* define function defines a module. It takes three parameters. Parameter 1 is the ID of this module.
  * Parameter 2 is the dependency list of this module, and parameter 3 is a factory function to create
  * the module. The ID of dynamic content scripts should be the same with its name, except that it 
@@ -36,56 +34,49 @@ define("#{{name}}", ["jquery", "#LibBase"], function(require, exports, module) {
   }
   {{name}}.prototype = libBase;
     
-  // Prototypes for {{name}}
-  {{name}}.prototype.method1 = function () {
-    console.log("{{name}}.method1 is invoked");
+  {{name}}.prototype.do = function () {
+    // Your code goes here.
+  	
+    console.log("{{name}}.do is invoked");
     console.log(this);
   };
   
-  // end of {{name}} prototypes
-  
   return new {{name}}();
 });
+
+${template_content_script_run}
 `;
 
 
-var template_content_script_common =
-`/* run function defines an entry point. It takes two parameters. Parameter 1 is the dependency list of this module.
- * Every element of this list is an ID of a dependent script. If the script is a dynamic content script, its ID
- * is the same with its name, except that it starts with a #. If there is no dependency, the first parameter can be omitted.
- * Parameter 2 is the function to to be called, and every dependency corresponds to a parameter of this function.
- */
-run(["jquery"], function($) {
-  // Your code here.
-});
-`;
-
-
-var template_content_script_module_def_run =
+var template_content_script_module_simple =
 `/* define function defines a module. It takes three parameters. Parameter 1 is the ID of this module.
  * Parameter 2 is the dependency list of this module, and parameter 3 is a factory function to create
  * the module. The ID of dynamic content scripts should be the same with its name, except that it 
  * starts with a #. The other dependencies should be defined in "seajs.config" section of Meta Data (Global).
  */
-define("#{{name}}", ["jquery", "#LibBase"], function(require, exports, module) {
+define("#{{name}}", ["jquery"], function(require, exports, module) {
   var $ = require("jquery");
-  var libBase = require("#LibBase");
-  
-  function {{name}}() {
-    this.config = this.getConfig("{{name}}");
-  }
-  {{name}}.prototype = libBase;
-    
-  // Prototypes for {{name}}
-  {{name}}.prototype.method1 = function () {
-    console.log("{{name}}.method1 is invoked");
-    console.log(this);
+
+  /* Exported symbols. You can define exports.whatever here and after. */
+  exports.do = function () {
+    // Your code goes here.
+  	
+    console.log("{{name}}.do is invoked");
   };
-  
-  // end of {{name}} prototypes
-  
-  return new {{name}}();
 });
 
-
+${template_content_script_run}
 `;
+
+
+var template_content_script_simple_run =
+`run(["jquery"], function($) {
+  // Your code here.
+});
+`;
+
+var template_content_script_all = {
+	"Module - Object Style"	: template_content_script_module_object,
+	"Module - Simple Style"	: template_content_script_module_simple,
+	"Simple Run"			: template_content_script_simple_run
+};
