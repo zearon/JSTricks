f4fb8930025b7a0567bee81cc1353a965a55bde2
ZHIYUAN GONG <register@zearon.com>
2015-12-13 13:17:10 +0800

    Workable. 1. Move msgbox.js, nodeSelector.js from usrlib dir to js dir. 2. Modify "request" plugin for Sea.js in boot.js to inject uri as id for anonymous module definition.

--- injected.js
+++ injected.js
@@ -1,19 +1,21 @@
-function __JSTricks_Injected_enableRightClick() {
-	function R(a) {
-		var ona = "on" + a;
-		if (window.addEventListener) window.addEventListener(a, 
-		function(e) {
-			for (var n = (e.originalTarget) ? e.originalTarget: e.target; n; n = n.parentNode) {
-				n[ona] = null;
-			}
-			/*console.log("event: " + ona); console.log(e.target);*/
-			
-		},
-		true);
-		window[ona] = null;
-		document[ona] = null;
-		if (document.body) document.body[ona] = null;
-	}
+function __JSTricks_Injected_removeEvent(a) {
+	var ona = "on" + a;
+	if (window.addEventListener) window.addEventListener(a, 
+	function(e) {
+		for (var n = (e.originalTarget) ? e.originalTarget: e.target; n; n = n.parentNode) {
+			n[ona] = null;
+		}
+		/*console.log("event: " + ona); console.log(e.target);*/
+		
+	},
+	true);
+	window[ona] = null;
+	document[ona] = null;
+	if (document.body) document.body[ona] = null;
+}
+
+function __JSTricks_Injected_enableRightClick() {	
+	var R = __JSTricks_Injected_removeEvent;
 	R("contextmenu");
 	R("click");
 	R("mousedown");
@@ -30,15 +32,15 @@ function __JSTricks_Injected_enableRightClick() {
 // process received message sent in content script by
 // window.postMessage({ type: "JST-callMethod", text: "enableRightClick" }, "*");
 window.addEventListener("message", function(event) {
-    console.log("Message received in injected.js: ");
+	console.log("Message received in injected.js: ");
 	console.log(event);
   // We only accept messages from ourselves
   if (event.source != window)
-    return;
+	return;
 
   if (event.data.type && (event.data.type == "JST-callMethod")) {
-    console.log("Invocation request received from Content script: " + event.data.text);
-    // call method __JSTricks_Injected_{method name}
-    window["__JSTricks_Injected_" + event.data.text]();
+	console.log("Invocation request received from Content script: " + event.data.text);
+	// call method __JSTricks_Injected_{method name}
+	window["__JSTricks_Injected_" + event.data.text]();
   }
 }, false);
